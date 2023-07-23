8c07550a66c3ce41b90d28e2d46fc2c81a4401ae
Filip Czaplicki <github@starsep.com>
2023-07-23 13:27:12 +0200

    Docker

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ Compares data of Roovee and OpenStreetMap.
 Code based on [NextbikeOSM](https://github.com/starsep/NextbikeOSM)
 
 Website at https://starsep.com/RooveeOSM/
+
+## Docker
+```
+docker build -t rooveeosm .
+docker run --rm \
+    -v "$(pwd)/cache:/app/cache" \
+    --env GITHUB_USERNAME=example \
+    --env GITHUB_TOKEN=12345 \
+    --env TZ=Europe/Warsaw \
+    -t rooveeosm
+```
--- update-server.sh
+++ update-server.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 set -eu
 date=$(/bin/date '+%Y%m%d')
-git pull
-rm -rf cache/overpass
-source .venv/bin/activate
 pip install -r requirements.txt
-cd output
-git pull
-cd ..
+git clone https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/starsep/RooveeOSM --depth 1 --branch gh-pages output
+rm -rf cache/overpass
 python main.py
 cd output
 git config user.name "RooveeOSMBot"
@@ -15,4 +11,3 @@ git config user.email "<>"
 git add .
 git commit -m "Update $date"
 git push origin gh-pages
-cd ..
