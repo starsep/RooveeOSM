bfac10b7f4602a53026d43a6f9d528282cbf32cd
Filip Czaplicki <github@starsep.com>
2025-05-10 19:40:53 +0200

    remove pip

--- update-server.sh
+++ update-server.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -eu
 date=$(/bin/date '+%Y%m%d')
-pip install -r requirements.txt
 git clone https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/starsep/RooveeOSM --depth 1 --branch gh-pages output
 rm -rf cache/overpass
 python main.py
